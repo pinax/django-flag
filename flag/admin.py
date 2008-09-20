70701c23b4b704f9e4e208507c9e1d8a90e88060
James Tauber <jtauber@jtauber.com>
2008-09-20 16:03:09 +0000

    improved admin with inlining
    git-svn-id: http://django-flag.googlecode.com/svn/trunk@25 6223c904-8705-11dd-a5c9-f1483cedc3eb

--- flag/admin.py
+++ flag/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from flag.models import FlaggedContent, FlagInstance
 
-admin.site.register(FlaggedContent)
-admin.site.register(FlagInstance)
+class InlineFlagInstance(admin.TabularInline):
+    model = FlagInstance
+    extra = 0
+
+class FlaggedContentAdmin(admin.ModelAdmin):
+    inlines = [InlineFlagInstance]
+
+admin.site.register(FlaggedContent, FlaggedContentAdmin)
