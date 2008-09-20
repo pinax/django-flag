44978d2d97fcb86e22d3bfdf0a946507165f6c4e
Greg Newman <greg@20seven.org>
2008-09-20 12:26:31 +0000

    adding forms, templates, templatetags and stubbing the view
    git-svn-id: http://django-flag.googlecode.com/svn/trunk@8 6223c904-8705-11dd-a5c9-f1483cedc3eb

--- flag/views.py
+++ flag/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+
+def flag(request):
+
